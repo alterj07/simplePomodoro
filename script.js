79a1583b6c7c7195e6b61761486166e74c31ee18
Jayden Chun <jayden.h.chun@gmail.com>
2026-04-16 18:03:55 -0500

    added functioning 3-hour w/ 10 minute breaks function

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-function timer(duration, display) {
-    var timer = duration, minutes, seconds;
-    const interval = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+function startTimer(duration, display) {
+    return new Promise((resolve) => {
+        var timer = duration, minutes, seconds;//duration is in seconds;
+        const interval = setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+            display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = 0;
-            clearInterval(interval);
-        }
-    }, 1000);
+            if (--timer < 0) {
+                timer = 0;
+                clearInterval(interval);
+                resolve();
+            }
+        }, 1000);
+    });
+}
+
+async function startBreak(display) {
+    await startTimer(10, display);
+}
+
+async function startThreeHourTimer(display) {
+    await startTimer(50, display);
+    await startBreak(display);
+    await startTimer(40, display);
+    await startBreak(display);
+    await startTimer(30, display);
+    await startBreak(display);
+    await startTimer(20, display);
+    await startBreak(display);
+    await startTimer(10, display);
+    await startBreak(display);
 }
