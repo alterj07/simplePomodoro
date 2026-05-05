272fa3aaf41e34637b4a35882e54d5f839b25d19
Jayden Chun <jayden.h.chun@gmail.com>
2026-05-05 14:29:49 -0500

    bug fix: multiple timers running @same time when clicking timer buttons before timer ran out

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+let timerRunning = false;
+
+
 function startTimer(duration, display) {
-    return new Promise((resolve) => {
-        var timer = duration, minutes, seconds;//duration is in seconds;
-        const interval = setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
+    if (!timerRunning){
+        return new Promise((resolve) => {
+            var timer = duration, minutes, seconds;//duration is in seconds;
+            timerRunning = true;
+            const interval = setInterval(function () {
+                minutes = parseInt(timer / 60, 10);
+                seconds = parseInt(timer % 60, 10);
 
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            display.textContent = minutes + ":" + seconds;
+                display.textContent = minutes + ":" + seconds;
 
-            if (--timer < 0) {
-                timer = 0;
-                clearInterval(interval);
-                resolve();
-            }
-        }, 1000);
-    });
+                if (--timer < 0) {
+                    timer = 0;
+                    timerRunning = false;
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, 1000);
+        });
+    }
 }
 
 async function startBreak(display) {
@@ -35,3 +42,4 @@ async function startThreeHourTimer(display) {
     await startTimer(10, display);
     await startBreak(display);
 }
+
